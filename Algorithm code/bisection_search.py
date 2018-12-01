db5e9e39e3db347265d7e99ae9f260d26d2704d1
trungnv33 <trung.vn1996@gmail.com>
2018-12-01 11:41:52 +0700

    fix bug bisection search :D

--- Algorithm code/bisection_search.py
+++ Algorithm code/bisection_search.py
@@ -3,23 +3,24 @@ def divide_half(list):
     first_half = (list[:n])
     second_half = (list[n:])
     return first_half,second_half
-divide_half([1,2,3,4,5,6,7])
 from merge_sort import mergeSort as ms
 def bisection_search(list,ele):
     list = ms(list)
-    start = 0
-    middle = len(list)//2
-    end  = len(list)
-    while ele < list[len(list)//2]:
-        list = divide_half(list)[0]
+    i,j = 0,0
+    while i<len(list) and j <len(list):
+        i,j =  0, 0
+        if ele<list[len(list)//2]:
+            list = divide_half(list)[0]
+            i+=1
+        else:
+            list = divide_half(list)[1]
+            j+=1
+        print(list)
+    if ele == list[0]:
+        return True
     else:
-        list = divide_half(list)[1]
-        if len(list)== 1:
-            if ele == list[0]:
-                return True
-            else:
-                return False
-print(bisection_search([1,2,3,4,5,6],3))
+        return False
+
 
          
 
